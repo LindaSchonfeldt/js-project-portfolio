2a95fcb2327ddb44bfc5ab229c6f531abd25165b
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-04-14 15:32:51 +0200

    added more css

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,25 +6,33 @@ export const HeroSection = () => {
     <section className='heroContainer'>
       <div className='heroContent'>
         <div>
-          <img src='assets/tree.svg' className='logoSmall' alt='' />
+          <img src='assets/tree.svg' className='logo' alt='' />
         </div>
         <h2 className='heroSubtitle'>I am Linda Schönfeldt</h2>
         <h1 className='heroTitle'>Web Developer with UX Skills</h1>
-        <p className='heroText'>
-          With a background in service and interaction design, I'm passionate
-          about building technology that truly understands user needs and
-          creates meaningful impact. What drives me is the desire to bring
-          clarity to complexity. I love organizing—whether it's structuring
-          information for better usability, designing intuitive user flows,
-          mapping out a product roadmap, or learning to write cleaner, more
-          maintainable code. Turning chaos into something clear, purposeful, and
-          human-centered is where I thrive. I'm especially drawn to
-          mission-driven teams that lead with empathy, value curiosity, and care
-          deeply about building thoughtful, empowering products.
-        </p>
+        <div className='heroText'>
+          <p>
+            With a background in service and interaction design, I'm passionate
+            about building technology that truly understands user needs and
+            creates meaningful impact.{' '}
+          </p>
+          <p>
+            What drives me is the desire to bring clarity to complexity. I{' '}
+            <b>love </b> organizing—whether it's structuring information for
+            better usability, designing intuitive user flows, mapping out a
+            product roadmap, or learning to write cleaner, more maintainable
+            code. Turning chaos into something clear, purposeful, and
+            human-centered is where I thrive.
+          </p>
+          <p>
+            I'm especially drawn to mission-driven teams that lead with empathy,
+            value curiosity, and care deeply about building thoughtful,
+            empowering products.
+          </p>
+        </div>
       </div>
-      <div className='imageLarge'>
-        <img src='assets/linda.svg' alt='' />
+      <div>
+        <img src='assets/linda.svg' className='heroImage' alt='' />
       </div>
     </section>
   )
