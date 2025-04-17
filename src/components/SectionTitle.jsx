5f10befc36f50ecd51a054d11c678fa0198719b2
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-04-17 11:09:35 +0200

    moved assets to public

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,7 +1,7 @@
 export const SectionTitle = ({ title, subtitle }) => {
   return (
     <div className='sectionTitle'>
-      <h2>{title}</h2>
+      <h2 className='sectionTitle'>{title}</h2>
       <p>{subtitle}</p>
     </div>
   )
