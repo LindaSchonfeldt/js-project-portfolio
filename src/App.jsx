43309304c5d57b7018fb29ca95a9e0641cf449f0
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-05-09 09:16:51 +0200

    removed ArticleSection from App

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { HeroSection } from './sections/HeroSection'
 import { SkillSection } from './sections/SkillSection'
 import { ProjectSection } from './sections/ProjectSection'
 import { TechSection } from './sections/TechSection'
-import { ArticleSection } from './sections/ArticleSection'
 import { ContactSection } from './sections/ContactSection'
 
 export const App = () => {
@@ -18,7 +17,6 @@ export const App = () => {
       <SkillSection />
       <ProjectSection projects={projects} />
       <TechSection content={content.techStack} />
-      <ArticleSection articles={articles.articles} />
       <ContactSection />
     </>
   )
